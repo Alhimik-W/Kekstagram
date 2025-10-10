68ea2c4dab8c86d689c39dcbe74b5a8088a89a1c
Dmitriy Uzhakhov <alhimik.web@yandex.ru>
2025-10-10 16:16:08 +0300

    Добавляет задание по функциям

--- js/functions.js
+++ js/functions.js
@@ -31,3 +31,26 @@ const extractNumbers = (string = '') => {
   return result === '' ? NaN : Number(result);
 };
 console.log(extractNumbers('Мне 38'));
+
+
+const isMeetingWork = (workStart, workEnd, meetingStart, meetingDuration) => {
+
+  const timeToMinutes = (time) => {
+    const [hours, minutes] = time.split(':').map(Number);
+
+    return hours * 60 + minutes;
+  };
+
+  const workStartMinutes = timeToMinutes(workStart);
+  const workEndMinutes = timeToMinutes(workEnd);
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= workStartMinutes && meetingEndMinutes <= workEndMinutes;
+};
+
+console.log(isMeetingWork('08:00', '17:30', '14:00', 90));
+console.log(isMeetingWork('8:0', '10:0', '8:0', 120));
+console.log(isMeetingWork('08:00', '14:30', '14:00', 90));
+console.log(isMeetingWork('14:00', '17:30', '08:0', 90));
+console.log(isMeetingWork('8:00', '17:30', '08:00', 900));
